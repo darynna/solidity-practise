ff088f874feed44275e26d432813eb38d02ce5f0
darynna <dgolikova0@gmail.com>
2024-03-23 11:58:04 +0000

    comit

--- src/Functions.sol
+++ src/Functions.sol
@@ -1,2 +1,25 @@
 pragma solidity ^0.8.0;
 
+// contract Demo {
+//     // 1)public 2)external 3)internal 4)private
+
+//     // view - only read but not modify - retunr some value;
+//     // pure
+//     string message = "Hello!"; //state
+
+//     function setMessage(string memory newMessage) external {
+//        message = newMessage;
+//     }
+
+//     //call
+//     function getBalance() public view returns(uint balance){
+//         balance = address(this).balance;
+//         //return balance;
+//     }
+
+//     function getMessage() external view returns(string memory){
+//         return message;
+//     }
+
+
+// }
